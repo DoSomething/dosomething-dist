a181eac0bbb76f420480e3b8de8f6b59b1ef5e3c
Jenkins <admin@dosomething.org>
2014-06-10 17:07:43 +0000

    Release v0.2.86

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/fact/node--fact_page.tpl.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Returns the HTML for the Fact page.
+ *
+ * Available Variables
+ * - $nid: Node ID for fact page (integer).
+ * - $classes: Additional classes passed for output (string).
+ * - $title: Title of fact page (string).
+ * - $subtitle: Subtitle of fact page (string).
+ * - $intro_title: Introductory title of fact page (string).
+ * - $facts: Array of facts for the fact page (array).
+ * - $sources: List of sources for the fact page (string).
+ * - $cta_link: Call To Action link of fact page (string).
+ */
+?>
+
+<article id="node-<?php print $node->nid; ?>" class="fact fact-page <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <header role="banner" class="-basic">
+    <div class="wrapper">
+      <h1 class="__title"><?php print $title; ?></h1>
+      <?php if (isset($subtitle)): ?>
+      <h2 class="__subtitle"><?php print $subtitle; ?></h2>
+      <?php endif; ?>
+    </div>
+  </header>
+
+  <?php if (isset($intro)): ?>
+    <section class="container intro">
+      <div class="wrapper">
+        <?php if (isset($intro_title)): ?>
+          <h2 class="container__title inline--alt-color"><?php print $intro_title; ?></h2>
+        <?php endif; ?>
+      </div>
+    </section>
+  <?php endif; ?>
+
+  <?php if (isset($facts)): ?>
+    <section class="container container--fact">
+
+      <div class="wrapper">
+
+        <div class="container__body">
+
+          <?php if (isset($intro_image)): ?>
+            <aside class="media -inline">
+               <?php print $intro_image; ?>
+            </aside>
+          <?php endif; ?>
+
+          <ol>
+            <?php foreach ($facts as $key => $fact): ?>
+              <li><?php print ($key + 1) . '. ' . $fact['fact']; ?>
+                <?php // @TODO: Sources reinstated, but not sure if the facts need to have numbers associated with their respective source? Need to clarify. ?>
+                <?php //<sup></?php print $fact['footnotes']; ?/></sup> ?>
+              </li>
+            <?php endforeach; ?>
+          </ol>            
+
+        </div>
+
+        <?php if (isset($sources)): ?>
+          <section class="sources">
+            <h3 class="__title  js-toggle-sources">Sources</h3>
+            <div class="__body legal">
+              <?php print $sources; ?>
+            </div>
+          </section>
+        <?php endif; ?>
+      </div>
+
+    </section>
+
+    <?php if (isset($call_to_action)): ?>
+      <div class="cta">
+        <div class="wrapper">
+          <h2 class="__message"><?php print $call_to_action; ?></h2>
+          <?php print $cta_link; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+    
+  <?php endif; ?>
+</article>
+
---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/static_content/node--static_content.tpl.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Returns the HTML for Static Content pages.
+ *
+ * Available Variables
+ * - $title: Title for the page (string).
+ * - $subtitle: Subtitle for the page (string).
+ */
+?>
+
+<section class="static_content-wrapper">
+  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+    <header role="banner" class="-basic<?php if (isset($sponsors[0]['display'])): print ' -sponsored'; endif; ?>">
+      <div class="wrapper">
+        <h1 class="__title"><?php print $title; ?></h1>
+        <?php if (isset($subtitle)): ?>
+        <h2 class="__subtitle"><?php print $subtitle; ?></h2>
+        <?php endif; ?>
+
+        <?php if (isset($sponsors[0]['display'])): ?>
+        <div class="sponsor">
+          <p class="__copy">Powered by</p>
+          <?php foreach ($sponsors as $key => $sponsor) :?>
+            <?php if (isset($sponsor['display'])): print $sponsor['display']; endif; ?>
+          <?php endforeach; ?>
+        </div>
+        <?php endif; ?>
+      </div>
+    </header>
+
+    <?php if (isset($intro)): ?>
+      <div class="intro-wrapper">
+        <div class="intro<?php if (!isset($intro_title)): print ' no-title'; endif; ?>">
+          <?php if (isset($intro_title)): ?>
+            <h2><?php print $intro_title; ?></h2>
+          <?php endif; ?>
+            <div class="intro-content<?php if (isset($intro_image) OR isset($intro_video)): print " intro-content-half-width"; endif; ?>"><?php print $intro; ?></div>
+          <?php if (isset($intro_image)): ?>
+            <?php print $intro_image; ?>
+          <?php elseif (isset($intro_video)): ?>
+            <?php print $intro_video; ?>
+          <?php endif; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <?php if (isset($call_to_action)): ?>
+      <div class="cta">
+        <div class="wrapper">
+          <h2 class="__message"><?php print $call_to_action; ?></h2>
+          <?php print $cta_link; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['field_blocks'])): ?>
+    <?php print render($content['field_blocks']); ?>
+    <?php endif; ?>
+
+    <?php if (!empty($galleries)): ?>
+      <div class="gallery-wrapper">
+        <?php foreach ($galleries as $gallery): ?>
+          <?php if (isset($gallery['title'])): ?>
+            <h2 class="gallery-title"><?php print $gallery['title']; ?></h2>
+          <?php endif; ?>
+          <div class="gallery">
+            <?php foreach ($gallery['items'] as $gallery_item): ?>
+              <div class="gallery-item">
+                <?php if (isset($gallery_item['image'])): ?>
+                  <?php if (isset($gallery_item['image_title']) AND $gallery_item['image_url'] !== '') : ?>
+                    <a href="<?php print $gallery_item['image_url']; ?>"><?php print $gallery_item['image']; ?></a>
+                  <?php else : ?>
+                    <?php print $gallery_item['image']; ?>
+                  <?php endif; ?>
+                <?php endif; ?>
+                <?php if (isset($gallery_item['image_title'])): ?>
+                  <h3 class="title"><?php print $gallery_item['image_title']; ?></h3>
+                <?php endif; ?>
+                <?php if (isset($gallery_item['image_description'])): ?>
+                  <div class="gallery-description"><?php print $gallery_item['image_description']; ?></div>
+                <?php endif; ?>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if (isset($additional_text)): ?>
+    <div class="additional-text-wrapper">
+      <div class="additional-text">
+        <?php if (isset($additional_text_title)): ?>
+          <h2><?php print $additional_text_title; ?></h2>
+        <?php endif; ?>
+
+        <p><?php print $additional_text; ?></p>
+      </div>
+    </div>
+    <?php endif; ?>
+
+    <?php if (isset($call_to_action)): ?>
+      <div class="cta">
+        <div class="wrapper">
+          <h2 class="__message"><?php print $call_to_action; ?></h2>
+          <?php print $cta_link; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <?php if (isset($sponsors)): ?>
+    <footer class="info-bar">
+      <div class="wrapper">
+        <?php if (isset($sponsors)): ?>
+          <div class="sponsor">
+            In partnership with <?php print $formatted_partners; ?>
+          </div>
+        <?php endif; ?>
+      </div>
+    </footer>
+    <?php endif; ?>
+
+  </article>
+</section>
