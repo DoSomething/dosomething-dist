7885a5ad132cf68807237047d0c142bcad9cf878
Jenkins <admin@dosomething.org>
2014-06-19 20:08:59 +0000

    Release v0.2.101

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-form-images.tpl.php
@@ -0,0 +1,6 @@
+<div class="submitted-image">
+  <p class="legal">Last updated: <?php print $updated; ?></p>
+  <?php foreach ($images as $image): ?>
+    <?php print $image; ?>
+  <?php endforeach; ?>
+</div>
