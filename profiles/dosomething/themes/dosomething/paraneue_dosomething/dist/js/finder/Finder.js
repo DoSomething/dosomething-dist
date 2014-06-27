2a136328ec39ec862cf91d65a35cd410f4e8b580
Jenkins <admin@dosomething.org>
2014-06-27 16:12:28 +0000

    Release v0.2.115

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/dist/js/finder/Finder.js
@@ -0,0 +1,75 @@
+define(function(require) {
+  "use strict";
+
+  var FormView = require("finder/FormView");
+  var ResultsView = require("finder/ResultsView");
+  var SolrAdapter = require("finder/SolrAdapter");
+
+  /**
+   * Finder manages the search action
+   *
+   * @type {Object}
+   */
+  var Finder = {
+
+    /**
+     * Initialize the Campaign Finder and, by extension, the Campaign Results handler
+     *
+     * @param  {JQuery} $formDiv          The div that the filters will be shown in
+     * @param  {JQuery} $resultsDiv       The div that the results will be injected into
+     * @param  {JQuery} $blankSlateDiv    The div that will be shown when no filter is selected
+     */
+    init: function ($formDiv, $resultsDiv, $blankSlateDiv) {
+      // Initialize the ResultsView
+      FormView.init($formDiv, Finder.query);
+      ResultsView.init($resultsDiv, $blankSlateDiv, Finder.query, FormView.clear);
+
+      // Prepare the Solr adapter
+      SolrAdapter.init();
+    },
+
+    /**
+     * Run the query against the Solr server
+     * @param  {int} offset   Offset to start results at (used for pagination).
+     */
+    query: function(offset) {
+      var queryOffset = offset || 0;
+      var checkedFields = FormView.getCheckedFields();
+
+      // Run the Solr query
+      if(!queryOffset) {
+        // If no offset, we display a new set of results
+        SolrAdapter.throttledQuery(checkedFields, queryOffset, Finder.displayResults);
+      } else {
+        // Otherwise, append to existing result set
+        SolrAdapter.throttledQuery(checkedFields, queryOffset, ResultsView.appendResults);
+      }
+
+      // Mark the results pane as loading
+      ResultsView.loading();
+    },
+
+    /**
+     * Displays results after querying Solr.
+     * @param data  Results from SolrAdapter
+     **/
+    displayResults: function(data) {
+      if (data.result) {
+        if(FormView.hasCheckedFields()) {
+          ResultsView.parseResults(data);
+          /* jshint ignore:start */
+          // Using that underscore variable notation for Solr.
+          FormView.disableFields(data.result["facet_counts"]["facet_queries"]);
+          /* jshint ignore:end */
+        } else {
+          ResultsView.showBlankSlate();
+          FormView.clear();
+        }
+      } else {
+        FormView.showErrorMessage();
+      }
+    }
+  };
+
+  return Finder;
+});
---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Returns the HTML for the Campaign Pitch page.
+ *
+ * Available Variables
+ * - $title: Title for the pitch page (string).
+ * - $cta: Call To Action for the pitch page (string).
+ * - $classes: Additional classes passed for output (string).
+ * - $scholarship: Scholarship amount (string).
+ */
+?>
+
+<section class="campaign campaign--pitch pitch">
+
+  <header role="banner" class="-hero <?php print $classes; ?>">
+    <div class="wrapper">
+      <h1 class="__title"><?php print $title; ?></h1>
+      <h2 class="__subtitle"><?php print $cta; ?></h2>
+
+      <?php if (isset($end_date)): ?><p class="__date"><?php print $end_date; ?></p><?php endif; ?>
+
+
+      <?php if (isset($signup_button)): ?>
+        <div class="__signup">
+          <?php print render($signup_button); ?>
+
+          <?php if (isset($scholarship)): ?>
+          <div class="message-callout -below -white -dynamic-right">
+            <div class="__copy">
+              <p><?php print $scholarship; ?></p>
+            </div>
+          </div>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (isset($sponsors[0]['display'])): ?>
+      <div class="sponsor">
+        <p class="__copy">Powered by</p>
+        <?php foreach ($sponsors as $key => $sponsor) :?>
+          <?php if (isset($sponsor['display'])): print $sponsor['display']; endif; ?>
+        <?php endforeach; ?>
+      </div>
+      <?php endif; ?>
+    </div>
+  </header>
+
+  <div class="wrapper">
+
+    <div class="container">
+      <div class="wrapper">
+        <p class="__tagline">A DoSomething.org Campaign. Join over 2.5 million young people taking action. Any cause, anytime, anywhere. <em>*Mic drop</em></p>
+      </div>
+    </div>
+
+  </div>
+
+</section>
