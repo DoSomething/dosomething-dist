9d843ce760d6f413de088658eb15f85b93fe782c
Jenkins <admin@dosomething.org>
2014-06-30 20:01:24 +0000

    Release v0.2.118

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/dist/js/validation/reportback.js
@@ -0,0 +1,36 @@
+define(function (require) {
+  "use strict";
+
+  var Validation= require("neue/validation");
+
+  // validate number of items
+  Validation.registerValidationFunction("reportbackNumber", function(string, done) {
+    if( string !== "" && /^[,0-9]+$/.test(string) ) {
+      return done({
+        success: true,
+        message: "That's great!"
+      });
+    } else {
+      return done({
+        success: false,
+        message: "Enter a number!"
+      });
+    }
+  });
+
+  // validate that string isn't blank
+  Validation.registerValidationFunction("reportbackReason", function(string, done) {
+    if( string !== "" ) {
+      return done({
+        success: true,
+        message: "Thanks for caring!"
+      });
+    } else {
+      return done({
+        success: false,
+        message: "Tell us why you cared!"
+      });
+    }
+  });
+
+});
