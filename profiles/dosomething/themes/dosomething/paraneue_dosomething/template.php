fcddc5c8240a4c0e587c912cde32b2261855dceb
Jenkins <admin@dosomething.org>
2014-07-21 19:22:34 +0000

    Release v0.2.145

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/template.php
@@ -0,0 +1,111 @@
+<?php
+
+// Define theme directory path
+define('PARANEUE_DS_PATH', drupal_get_path('theme', 'paraneue_dosomething'));
+define('LOCAL_ASSET_PATH', '/' . PARANEUE_DS_PATH . '/dist');
+
+// If ds_version is set, use CDN assets
+if(theme_get_setting('asset_path')) {
+  $cdn_path = theme_get_setting('asset_path');
+  $cdn_path = str_replace('{ds_version}', variable_get('ds_version', 'latest'), $cdn_path);
+  define('DS_ASSET_PATH', $cdn_path);
+} else {
+  define('DS_ASSET_PATH', LOCAL_ASSET_PATH);
+}
+
+// Determine whether to use minified script or not
+if(theme_get_setting('use_minified_assets')) {
+  define('DS_STYLE_PATH', DS_ASSET_PATH . '/app.min.css');
+  define('DS_JAVASCRIPT_PATH', DS_ASSET_PATH . '/app.min.js');
+} else {
+  define('DS_STYLE_PATH', DS_ASSET_PATH . '/app.css');
+  define('DS_JAVASCRIPT_PATH', DS_ASSET_PATH . '/app.js');
+}
+
+// Define asset directory paths
+define('VENDOR_ASSET_PATH', DS_ASSET_PATH . '/bower_components');
+define('NEUE_ASSET_PATH', VENDOR_ASSET_PATH . '/neue');
+
+// Theme includes
+require_once PARANEUE_DS_PATH . '/includes/bootstrap.inc';
+require_once PARANEUE_DS_PATH . '/includes/theme.inc';
+require_once PARANEUE_DS_PATH . '/includes/preprocess.inc';
+require_once PARANEUE_DS_PATH . '/includes/helpers.inc';
+require_once PARANEUE_DS_PATH . '/includes/form.inc';
+require_once PARANEUE_DS_PATH . '/includes/auth/login.inc';
+require_once PARANEUE_DS_PATH . '/includes/auth/register.inc';
+
+
+/**
+ * We use our own asset pipeline (through Grunt), so we want to bypass most
+ * of Drupal's included assets.
+ *
+ * Implements hook_css_alter().
+ */
+function paraneue_dosomething_css_alter(&$css) {
+  // Load excluded CSS files from theme.
+  $excludes = _paraneue_dosomething_alter(paraneue_dosomething_theme_get_info('exclude'), 'css');
+  $css = array_diff_key($css, $excludes);
+
+  // Force removal of files we can't seem to kill with exclude[css] array
+  // @TODO: Figure out why this is necessary.
+  unset($css[drupal_get_path('module', 'date') . '/date_api/date.css']);
+  unset($css[drupal_get_path('module', 'ctools') . '/css/ctools.css']);
+  unset($css[drupal_get_path('module', 'views') . '/css/views.css']);
+}
+
+/**
+ * We use our own asset pipeline (through Grunt), so we want to bypass most
+ * of Drupal's included assets.
+ *
+ * Implements hook_js_alter().
+ */
+function paraneue_dosomething_js_alter(&$js) {
+  // Load excluded JS files from theme.
+  $excludes = _paraneue_dosomething_alter(paraneue_dosomething_theme_get_info('exclude'), 'js');
+  $js = array_diff_key($js, $excludes);
+
+  // Force removal of files that we can't seem to kill with exclude[js] array
+  unset($js['profiles/dosomething/modules/contrib/devel/devel_krumo_path.js']);
+}
+
+
+/**
+ * Implements hook_page_alter().
+ */
+function paraneue_dosomething_page_alter(&$page) {
+  // Ensure we have a 'page_bottom' region
+  if (!isset($page['page_bottom'])) {
+    $page['page_bottom'] = array('region' => 'page_bottom');
+  }
+
+  paraneue_dosomething_page_alter_login($page);
+  paraneue_dosomething_page_alter_register($page);
+}
+
+/**
+ * Implements hook_form_alter();
+ */
+function paraneue_dosomething_form_alter(&$form, &$form_state, $form_id) {
+  paraneue_dosomething_form_alter_base($form, $form_state, $form_id);
+  paraneue_dosomething_form_alter_login($form, $form_state, $form_id);
+  paraneue_dosomething_form_alter_register($form, $form_state, $form_id);
+}
+
+
+/**
+ * Implements hook_head_alter().
+ */
+function paraneue_dosomething_html_head_alter(&$head_elements) {
+  foreach($head_elements as $key => $element) {
+    switch($key) {
+      case (preg_match('/^drupal_add_html_head_link*/', $key)) ? TRUE : FALSE:
+        $shortcut_key = $key;
+        break;
+    }
+  }
+
+  if ($shortcut_key) {
+    unset($head_elements[$shortcut_key]);
+  }
+}
---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/system/partials/footer.tpl.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Generates site-wide global page footer.
+ **/
+
+  // Checks if current site is an international affiliate
+  $is_affiliate = dosomething_settings_is_affiliate();
+?>
+
+<footer class="chrome--footer<?php if ($is_affiliate): ?> -affiliate <?php endif; ?>">
+
+  <?php if (isset($affiliate_logo)): ?>
+  <div class="col branding">
+    <h4><?php print $affiliate_logo['text'] ?></h4>
+    <?php 
+      print theme_image(array(
+        'path'  => $affiliate_logo['file'],
+        'title' => $affiliate_logo['text'],
+        'alt'   => $affiliate_logo['text'],
+      )); 
+    ?>
+  </div>
+  <?php endif; ?>
+
+  <div class="social tablet">
+    <ul>
+      <?php if ($social['facebook'] !== 0): ?>
+      <li><a class="social-link" title="dosomething on Facebook" href="https://www.facebook.com/dosomething">&#xe600;</a></li>
+      <?php endif; ?>
+      <?php if ($social['twitter'] !== 0): ?>
+      <li><a class="social-link" title="@dosomething on Twitter" href="https://twitter.com/dosomething">&#xe603;</a></li>
+      <?php endif; ?>
+      <?php if ($social['tumblr'] !== 0): ?>
+      <li><a class="social-link" title="@dosomething on Tumblr" href="http://dosomething.tumblr.com">&#xe602;</a></li>
+      <?php endif; ?>
+      <?php if ($social['instagram'] !== 0): ?>
+      <li><a class="social-link" title="@dosomething on Instagram" href="http://instagram.com/dosomething">&#xe604;</a></li>
+      <?php endif; ?>
+      <?php if ($social['youtube'] !== 0): ?>
+      <li><a class="social-link" title="dosomething1 on YouTube" href="http://www.youtube.com/dosomething1">&#xe601;</a></li>
+      <?php endif; ?>
+    </ul>
+  </div>
+
+  <?php foreach($links as $column): ?>
+  <div class="col <?php print $column['class']; ?> js-footer-col">
+    <h4><?php print $column['heading']; ?></h4>
+    <?php if (!empty($column['links'])): ?>
+    <ul>
+      <?php foreach($column['links'] as $link): ?>
+        <li><?php print $link; ?></li>
+      <?php endforeach; ?>
+    </ul>
+    <?php endif; ?>
+  </div>
+  <?php endforeach; ?>
+
+  <div class="social mobile">
+    <ul>
+      <?php if ($social['facebook'] !== 0): ?>
+      <li><a class="social-link" title="dosomething on Facebook" href="https://www.facebook.com/dosomething">&#xe600;</a></li>
+      <?php endif; ?>
+      <?php if ($social['twitter'] !== 0): ?>
+      <li><a class="social-link" title="@dosomething on Twitter" href="https://twitter.com/dosomething">&#xe603;</a></li>
+      <?php endif; ?>
+      <?php if ($social['tumblr'] !== 0): ?>
+      <li><a class="social-link" title="@dosomething on Tumblr" href="http://dosomething.tumblr.com">&#xe602;</a></li>
+      <?php endif; ?>
+      <?php if ($social['instagram'] !== 0): ?>
+      <li><a class="social-link" title="@dosomething on Instagram" href="http://instagram.com/dosomething">&#xe604;</a></li>
+      <?php endif; ?>
+      <?php if ($social['youtube'] !== 0): ?>
+      <li><a class="social-link" title="dosomething1 on YouTube" href="http://www.youtube.com/dosomething1">&#xe601;</a></li>
+      <?php endif; ?>
+    </ul>
+  </div>
+
+  <div class="subfooter">
+    <ul class="utility">
+      <li><?php print l("Terms of Service", "node/1049") ?></li>
+      <li><?php print l("Privacy Policy", "node/1050") ?></li>
+    </ul>
+  </div>
+</footer>
