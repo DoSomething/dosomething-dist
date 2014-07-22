d16ac0b9bf9d154fc86f9601963525dfabebc83e
Jenkins <admin@dosomething.org>
2014-07-22 15:05:31 +0000

    Release v0.2.146

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--closed.tpl.php
@@ -0,0 +1,293 @@
+<?php
+/**
+ * Returns the HTML for the Campaign Closed page.
+ *
+ * Available Variables
+ * - $campaign: A campaign object. @see dosomething_campaign_load()
+ * - $title: Title for the campaign closed page (string).
+ * - $classes: Additional classes passed for output (string).
+ * - $presignup_callout: Call to action (string).
+ * - $sponsors: List of sponsors (array).
+ * - $total_participants: Number of members participated (string).
+ * - $total_quantity: Total quantity of campaign items (string).
+ * - $total_quantity_label: Label for the campign items donated (string).
+ * - $intro: Intro copy for campaign closed page (string).
+ * - $reportback_gallery: Galleries for 'What You Did' (array).
+ */
+?>
+
+<article class="campaign campaign--closed closed">
+
+  <header role="banner" class="-hero <?php print $classes; ?>">
+    <div class="wrapper">
+      <?php print $campaign_headings; ?>
+
+      <?php if (isset($signup_button)): ?>
+        <div class="__signup">
+          <?php print render($signup_button); ?>
+
+          <?php if (isset($presignup_callout)): ?>
+          <div class="message-callout -below -white -dynamic-right">
+            <div class="__copy">
+              <p><?php print $presignup_callout; ?></p>
+            </div>
+          </div>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (isset($sponsors[0]['display'])): ?>
+      <div class="sponsor">
+        <p class="__copy">Powered by</p>
+        <?php foreach ($sponsors as $key => $sponsor) :?>
+          <?php if (isset($sponsor['display'])): print $sponsor['display']; endif; ?>
+        <?php endforeach; ?>
+      </div>
+      <?php endif; ?>
+
+    </div>
+  </header>
+
+  <?php // WHAT YOU DID ////////////////////////////////////////////////////// ?>
+  <section class="container container--did">
+    <h2 class="container__title banner"><span>What You Did</span></h2>
+
+    <div class="wrapper">
+
+      <div class="container__body">
+
+        <?php // Campaign statistics ?>
+
+        <div class="__row">
+          <?php if (isset($total_participants)): ?>
+
+            <?php // Number of members  participated ?>
+            <div class="statistic<?php if ($participants_columned): ?> -columned -odd<?php endif; ?>">
+              <div class="statistic__body">
+                <p>
+                  <strong class="inline--alt-color"><?php print $total_participants; ?></strong>
+                  <em>Members Participated</em>
+                </p>
+              </div>
+            </div>
+          <?php endif; ?>
+
+          <?php if (isset($total_quantity_label)) : ?>
+
+            <?php if (isset($total_quantity)): ?>
+
+              <?php // Total quantity & label ?>
+              <div class="statistic<?php if ($quantity_columned): ?> -columned -even -col-last<?php endif; ?>">
+                <div class="statistic__body">
+                  <p>
+                    <strong class="inline--alt-color"><?php print $total_quantity; ?></strong>
+                    <em><?php print $total_quantity_label; ?></em>
+                  </p>
+                </div>
+              </div>
+
+            <?php else: ?>
+
+              <?php if (isset($total_quantity_placeholder)): ?>
+
+                <?php // Placeholder copy when quantity has not been entered ?>
+                <div class="statistic<?php if ($quantity_columned): ?> -columned -even -col-last<?php endif; ?>">
+                  <div class="statistic__body">
+                    <div class="placeholder">
+                      <p><?php print $total_quantity_placeholder; ?></p>
+                    </div>
+                  </div>
+                </div>
+
+              <?php endif; ?>
+
+            <?php endif; ?>
+
+          <?php endif; ?>
+        </div>
+
+        <?php // Intro copy ?>
+        <?php if (isset($intro)): ?>
+          <div class="intro">
+            <?php print $intro['safe_value']; ?>
+          </div>
+        <?php endif; ?>
+
+        <?php // Reportback gallery ?>
+        <?php if (isset($reportback_gallery)): ?>
+          <ul class="gallery -triad">
+            <?php foreach ($reportback_gallery as $key => $reportback_gallery_item) :?>
+
+              <li class="<?php print $reportback_gallery_item['order_class']; ?>">
+                <div class="tile tile--figure">
+                  <?php if (isset($reportback_gallery_item['image'])): ?>
+                    <?php print $reportback_gallery_item['image']; ?>
+                  <?php endif; ?>
+                  <?php if (isset($reportback_gallery_item['first_name'])): ?>
+                    <h3 class="__title"><?php print $reportback_gallery_item['first_name']; ?></h3>
+                  <?php endif; ?>
+                  <?php if (isset($reportback_gallery_item['caption'])): ?>
+                    <div class="__description"><?php print $reportback_gallery_item['caption']; ?></div>
+                  <?php endif; ?>
+                </div>
+              </li>
+
+            <?php endforeach; ?>
+          </ul>
+        <?php endif; ?>
+
+      </div>
+    </div>
+  </section>
+
+  <?php if ($display_buzz_section): ?>
+  <?php // THE BUZZ ////////////////////////////////////////////////////// ?>
+    <section class="container container--celebs">
+      <h2 class="container__title banner"><span>The Buzz</span></h2>
+      <div class="wrapper">
+
+        <div class="container__body">
+
+          <div class="__row -psa">
+            <div <?php if (isset($psa)): ?>class="-columned -odd"<?php endif; ?>>
+              <?php if (isset($additional_text_title)): ?>
+              <h3 class="inline--alt-color"><?php print $additional_text_title; ?></h3>
+              <?php endif; ?>
+
+              <?php if (isset($additional_text)): ?>
+              <div><?php print $additional_text['safe_value']; ?></div>
+              <?php endif; ?>
+            </div>
+
+            <?php if (isset($psa)): ?>
+              <div class="-columned -col-last">
+                <aside class="video">
+                  <?php print $psa; ?>
+                </aside>
+              </div>
+            <?php endif; ?>
+          </div>
+
+          <?php foreach ($klout_gallery as $key => $klout_gallery_item) :?>
+
+            <h3 class="inline--alt-color"><?php print $klout_gallery_item['title']; ?></h3>
+
+            <?php // The klout galleries ?>
+            <ul class="gallery <?php print $klout_gallery_item['style']; ?>">
+              <?php foreach ($klout_gallery_item['items'] as $key => $gallery_item) :?>
+
+                <?php if ($klout_gallery_item['type'] === 'mention') : ?>
+                  <li class="<?php print $gallery_item['order_class']; ?>">
+                    <div class="tile tile--figure">
+                      <?php if (isset($gallery_item['image'])): ?>
+                      <div class="__media">
+                        <?php if (isset($gallery_item['url']) && !empty($gallery_item['url'])): ?>
+                          <a href="<?php print $gallery_item['url']; ?>"><?php print $gallery_item['image']; ?></a>
+                        <?php else: ?>
+                          <?php print $gallery_item['image']; ?>
+                        <?php endif; ?>
+                      </div>
+                      <?php endif; ?>
+
+                      <div class="__body">
+                        <?php if (isset($gallery_item['title']) && !empty($gallery_item['title'])): ?>
+                          <h3 class="__title"><?php print $gallery_item['title']; ?></h3>
+                        <?php endif; ?>
+                        <?php if (isset($gallery_item['desc'])): ?>
+                          <div class="__description"><?php print $gallery_item['desc']; ?></div>
+                        <?php endif; ?>
+                      </div>
+                    </div>
+                  </li>
+                <?php else: ?>
+                  <li class="<?php print $gallery_item['order_class']; ?>">
+                    <div class="tile tile--figure">
+                      <?php if (isset($gallery_item['image'])): ?>
+                        <div class="__media">
+                          <?php if (isset($gallery_item['url']) && !empty($gallery_item['url'])): ?>
+                            <a href="<?php print $gallery_item['url']; ?>"><?php print $gallery_item['image']; ?></a>
+                          <?php else: ?>
+                            <?php print $gallery_item['image']; ?>
+                          <?php endif; ?>
+                        </div>
+                      <?php endif; ?>
+                      <?php if (isset($gallery_item['title']) && !empty($gallery_item['title'])): ?>
+                        <h3 class="__title"><?php print $gallery_item['title']; ?></h3>
+                      <?php endif; ?>
+                      <?php if (isset($gallery_item['desc'])): ?>
+                        <div class="__description"><?php print $gallery_item['desc']; ?></div>
+                      <?php endif; ?>
+                    </div>
+                  </li>
+                <?php endif; ?>
+
+              <?php endforeach; ?>
+
+            </ul>
+
+          <?php endforeach; ?>
+
+        </div>
+
+      </div>
+    </section>
+  <?php endif; ?>
+
+  <?php // CONGRATULATIONS TO... ////////////////////////////////////////////////////// ?>
+  <section class="container container--congrats">
+    <h2 class="container__title banner"><span>Congrats to&hellip;</span></h2>
+    <div class="wrapper">
+
+      <div class="container__body">
+
+        <?php // Winners ?>
+
+        <?php // If winners have been picked, display as a gallery ?>
+        <?php if (isset($winners)): ?>
+
+          <?php foreach ($winners as $key => $winner) :?>
+            <div class="__row">
+              <div <?php if (isset($winner['image'])): ?>class="-columned"<?php endif; ?>>
+                <?php if (isset($winner['fname'])): ?>
+                  <h3 class="inline--alt-color"><?php print $winner['fname']; ?></h3>
+                <?php endif; ?>
+
+                <?php if (isset($winner['field_winner_type'])): ?>
+                  <h4><?php print $winner['field_winner_type']; ?> winner</h4>
+                <?php endif; ?>
+
+
+                <?php if (isset($winner['field_winner_description'])): ?>
+                  <p><?php print $winner['field_winner_description']; ?></p>
+                <?php endif; ?>
+
+                <?php if (isset($winner['field_winner_quote'])): ?>
+                  <p>"<?php print $winner['field_winner_quote']; ?>"</p>
+                <?php endif; ?>
+              </div>
+
+              <?php if (isset($winner['image'])): ?>
+                <aside class="-columned -col-last">
+                  <?php print $winner['image']; ?>
+                </aside>
+              <?php endif; ?>
+            </div>
+          <?php endforeach; ?>
+
+      <?php // Else display the default placeholder copy while winners are being chosen ?>
+      <?php elseif (isset($default_winners)) : ?>
+        <div class="placeholder">
+          <p><?php print $default_winners; ?></p>
+        </div>
+      <?php endif; ?>
+
+      </div>
+
+    </div>
+  </section>
+
+  <?php if ($info_bar): ?>
+    <?php print $info_bar; ?>
+  <?php endif; ?>
+
+</article>
---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--sms-game.tpl.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Returns the HTML for the Campaign SMS Game page.
+ *
+ * Available Variables
+ * - $campaign: A campaign object. @see dosomething_campaign_load()
+ * - $scholarship: Scholarship amount (string).
+ * - $classes: Additional classes passed for output (string).
+ */
+?>
+
+<article id="node-<?php print $node->nid; ?>" class="campaign campaign--sms <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <header role="banner" class="-hero <?php print $classes; ?>">
+    <div class="wrapper">
+      <?php print $campaign_headings; ?>
+
+      <?php if (isset($sponsors[0]['display'])): ?>
+      <div class="sponsor">
+        <p class="__copy">Powered by</p>
+        <?php foreach ($sponsors as $key => $sponsor) :?>
+          <?php if (isset($sponsor['display'])): print $sponsor['display']; endif; ?>
+        <?php endforeach; ?>
+      </div>
+      <?php endif; ?>
+    </div>
+  </header>
+
+  <div class="wrapper">
+
+    <section id="know" class="container know">
+      <h2 class="container__title banner"><span>Step 1: Know It</span></h2>
+
+      <div class="wrapper">
+
+        <div class="container__body">
+          <div class="-columned">
+            <?php if (isset($campaign->fact_problem)): ?>
+            <h3 class="inline--alt-color">The Problem</h3>
+            <p><?php print $campaign->fact_problem['fact']; ?><sup><?php print $campaign->fact_problem['footnotes']; ?></sup></p>
+            <?php endif; ?>
+
+            <?php if (isset($psa)): ?>
+              <aside <?php if ($is_video_psa) echo 'class="video"'; ?>>
+                <?php print $psa; ?>
+              </aside>
+            <?php else: ?>
+              <?php if (isset($modals)): ?>
+                <?php print $modals; ?>
+              <?php endif; ?>
+            <?php endif; ?>
+          </div>
+
+          <div class="-columned -col-last">
+            <?php if (isset($campaign->fact_solution) || isset($campaign->solution_copy)): ?>
+                <h3 class="inline--alt-color">The Solution</h3>
+
+              <?php if (isset($campaign->fact_solution)): ?>
+                <p><?php print $campaign->fact_solution['fact']; ?><sup><?php print $campaign->fact_solution['footnotes']; ?></sup></p>
+              <?php elseif (isset($campaign->solution_copy)): ?>
+                <?php print $campaign->solution_copy; ?>
+              <?php endif; ?>
+
+              <?php if (isset($campaign->solution_support)): ?>
+                <?php print $campaign->solution_support; ?>
+              <?php endif; ?>
+
+            <?php endif; ?>
+
+            <?php if (isset($psa)): ?>
+              <?php if (isset($modals)): ?>
+                <?php print $modals; ?>
+              <?php endif; ?>
+            <?php endif; ?>
+          </div>
+        </div>
+
+
+        <?php if (isset($campaign->fact_sources)): ?>
+        <section class="sources">
+          <h3 class="__title js-toggle-sources">Sources</h3>
+          <ul class="__body legal">
+            <?php foreach ($campaign->fact_sources as $key => $source): ?>
+              <li><sup><?php print ($key + 1); ?></sup> <?php print $source; ?></li>
+            <?php endforeach; ?>
+          </ul>
+        </section>
+        <?php endif; ?>
+      </div>
+    </section>
+
+
+    <section id="do" class="container container--do inline--alt-bg">
+      <h2 class="container__title banner"><span>Step 2: Do It</span></h2>
+
+      <div class="wrapper">
+        <div class="container__body">
+          <?php if (isset($starter_header)) : ?>
+            <h3 class="inline--alt-color"><?php print $starter_header; ?></h3>
+          <?php endif; ?>
+          <?php if (isset($starter)) : ?>
+            <div><?php print $starter['safe_value']; ?></div>
+          <?php endif; ?>
+
+          <?php if (isset($signup_form)) : ?>
+            <?php print render($signup_form); ?>
+          <?php endif; ?>
+
+          <?php if (isset($scholarship)): ?>
+          <div class="message-callout -right">
+            <div class="__copy">
+              <p><?php print $scholarship; ?></p>
+            </div>
+          </div>
+          <?php endif; ?>
+        </div>
+
+        <div class="disclaimer legal">
+          <p>Taking part in this experience means you agree to our <a href="https://www.dosomething.org/about/terms-service">Terms of Service</a> &amp; to receive our weekly update. Message & data rates may apply. Text STOP to opt-out, HELP for help.</p>
+        </div>
+      </div>
+    </section>
+
+    <?php if ($info_bar): ?>
+      <?php print $info_bar; ?>
+    <?php endif; ?>
+
+  </div>
+
+</article>
