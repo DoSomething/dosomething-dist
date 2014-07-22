9e3127860548393d91b0e33ec4271f6c0f4ad9c5
Jenkins <admin@dosomething.org>
2014-07-22 19:18:00 +0000

    Release v0.2.147

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/js/validation/address.js
@@ -0,0 +1,155 @@
+define(function(require) {
+  "use strict";
+
+  var $ = require("jquery");
+  var Validation= require("neue/validation");
+
+  /** @NOTE: Temporary, testing out some new ideas. */
+  var requiredValidator = function(string, done, args) {
+    if( string !== "" ) {
+      return done({
+        success: true,
+        message: args.success
+      });
+    } else {
+      return done({
+        success: false,
+        message: args.failure
+      });
+    }
+  };
+
+  /**
+   * Validators for individual address form fields:
+   */
+  Validation.registerValidationFunction("fname", function(string, done) {
+    return requiredValidator(string, done, {
+      success: "Oh hey, " + string + "!",
+      failure: "We need your name. We’re on a first-name basis, right?"
+    });
+  });
+
+  Validation.registerValidationFunction("lname", function(string, done) {
+    return requiredValidator(string, done, {
+      success: "The " + string + "-inator! People call you that, right?",
+      failure: "We need your last name."
+
+    });
+  });
+
+  Validation.registerValidationFunction("address1", function(string, done) {
+    return requiredValidator(string, done, {
+      success: "Got it!",
+      failure: "We need your street name and number."
+    });
+  });
+
+  Validation.registerValidationFunction("address2", function(string, done) {
+    return requiredValidator(string, done, {
+      success: "Got that too!",
+      failure: ""
+    });
+  });
+
+  Validation.registerValidationFunction("city", function(string, done) {
+    return requiredValidator(string, done, {
+      success: "Sweet, thanks!",
+      failure: "We need your city."
+    });
+  });
+
+  Validation.registerValidationFunction("state", function(string, done) {
+    return requiredValidator(string, done, {
+      success: "I \u2764 " + string,
+      failure: "We need your state."
+    });
+  });
+
+  Validation.registerValidationFunction("zipcode", function(string, done) {
+    if( string.match(/^\d{5}(?:[-\s]\d{4})?$/) ) {
+      return done({
+        success: true,
+        message: "Almost done!"
+      });
+    } else {
+      return done({
+        success: false,
+        message: "We need your zip code."
+      });
+    }
+  });
+
+  Validation.registerValidationFunction("why_signedup", function(string, done) {
+    return requiredValidator(string, done, {
+      success: "Thanks for caring!",
+      failure: "Oops! Can't leave this one blank."
+    });
+  });
+
+
+  /**
+   * Custom validation for UPS address fieldset.
+   */
+  Validation.registerValidationFunction("ups_address", function($el, done) {
+    var $sorryError = $("<div class='messages error'><strong>We couldn't find that address.</strong> Double check for typos and try submitting again.</div>");
+    var $networkError= $("<div class='messages error'>We're having trouble submitting the form, are you sure your internet connection is working? Email us if you continue having problems.</div>");
+    var addressFieldData = $el.find("select, input").serializeArray();
+
+    // Remove previous messages.
+    $el.find(".messages").slideUp(function() { $(this).remove(); });
+
+    // Once we know we have, submit them to the AJAX endpoint
+    $.ajax({
+      type: "POST",
+      url: "/user/validate/address",
+      dataType: "json",
+      data: addressFieldData,
+      success: function(data) {
+        console.log(data);
+
+        if(data.sorry) {
+          // Address is invalid and we don't have any suggestions
+          $el.append($sorryError).hide().slideDown();
+          return done({ success: false });
+        }
+
+        var hasFieldErrors = false;
+        for(var field in data) {
+          if(data.hasOwnProperty(field) && field !== "ambiguous") {
+            var suggestion = data[field];
+            var $fieldEl = $el.find("[name='user_address[" + field + "]']");
+
+            // If just capitalizing or adding extra 4-digit to zip, don't show an error
+            if(  (field === "postal_code" && $fieldEl.val().slice(0,4) === suggestion.slice(0,4)) || (suggestion === $fieldEl.val().toUpperCase() ) ) {
+              $fieldEl.val( suggestion  );
+            } else {
+              // Otherwise, mark errors and ask user to correct them
+              hasFieldErrors = true;
+
+              Validation.showValidationMessage($fieldEl, {
+                success: false,
+                suggestion: {
+                  full: suggestion,
+                  domain: "zip"
+                }
+              });
+            }
+          }
+        }
+
+        if(hasFieldErrors) {
+          done({ success: false });
+        } else {
+          done({ success: true });
+        }
+
+      },
+      error: function() {
+        // We're having trouble getting the validation over AJAX, let's mark as
+        // validated and leave it up to the server just to be safe.
+        $el.append($networkError).hide().slideDown();
+        done({ success: false });
+      }
+    });
+  });
+});
---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Returns the HTML for the Campaign Pitch page.
+ *
+ * Available Variables
+ * - $campaign: A campaign object. @see dosomething_campaign_load()
+ * - $classes: Additional classes passed for output (string).
+ * - $scholarship: Scholarship amount (string).
+ */
+?>
+
+<section class="campaign campaign--pitch pitch">
+
+  <header role="banner" class="-hero <?php print $classes; ?>">
+    <div class="wrapper">
+      <?php print $campaign_headings; ?>
+
+      <?php if (isset($signup_button)): ?>
+        <div class="__signup">
+          <?php print render($signup_button); ?>
+          <?php print $campaign_scholarship; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (isset($sponsors[0]['display'])): ?>
+      <div class="sponsor">
+        <p class="__copy">Powered by</p>
+        <?php foreach ($sponsors as $key => $sponsor) :?>
+          <?php if (isset($sponsor['display'])): print $sponsor['display']; endif; ?>
+        <?php endforeach; ?>
+      </div>
+      <?php endif; ?>
+    </div>
+  </header>
+
+  <div class="wrapper">
+
+    <div class="container container--tagline">
+      <div class="wrapper">
+        <p class="__tagline">A DoSomething.org Campaign. Join over 2.5 million young people taking action. Any cause, anytime, anywhere. <em>*Mic drop</em></p>
+      </div>
+    </div>
+
+  </div>
+
+</section>
