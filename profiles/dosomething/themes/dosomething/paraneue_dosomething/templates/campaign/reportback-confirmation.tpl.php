7d03ec146ba9750899a355d7f9c7c46c4ef9b051
Jenkins <admin@dosomething.org>
2014-08-11 18:09:19 +0000

    Release v0.2.196

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/reportback-confirmation.tpl.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Returns the HTML for the Campaign Reportback Confirmation.
+ *
+ * Available Variables
+ * - $page_title: The page title (string).
+ * - $copy: Positive note message copy (string).
+ * - $more_campaigns_link: Link to find more campaigns (string).
+ * - $back_to_campaign_link: Link to head back to originating campagin (string).
+ * - $recommended: Array containing recommended campaigns for user.
+ *   - [nid]: Node ID for campaign (integer).
+ *   - [title]: Title of campaign (string).
+ *   - [call_to_action]: Call to action text for campaign. (string).
+ *   - [image]: URL path for campaign image (string).
+ *   - [path]: URL path for campaign node (string).
+ *   - [pretty_path]: Pretty URL path for campaign node (string).
+ *   - [staff_pick]: Indicate if this campaign a staff pick (boolean).
+ * - $redeem_form: Array containing variables for Reward Reedem Form, if exists:
+ *   - [copy]: Copy displayed in the Redeem Form modal (string).
+ *   - [delete_form]: If staff, the Reward Delete Form to be rendered (array).
+ *   - [form]: Reward Reedem Form to be rendered (array).
+ *   - [header]: Header text of the Redeem Form modal (string).
+ *   - [link]: Label of link to open the Redeem form modal (string).
+ */
+?>
+
+<section class="confirmation-wrapper">
+
+  <header role="banner" class="-basic">
+    <div class="wrapper">
+      <h1 class="__title"><?php print $page_title; ?></h1>
+      <?php if (isset($copy)): ?>
+        <h2 class="__subtitle"><?php print $copy; ?></h2>
+      <?php endif; ?>
+    </div>
+  </header>
+
+  <?php if (isset($recommended)): ?>
+    <div class="gallery-wrapper">
+      <h2 class="gallery-title"><?php print t('Keep it up! Find your next campaign.'); ?></h2>
+      <div class="gallery">
+        <?php foreach ($recommended as $rec): ?>
+          <div class="gallery-item">
+            <?php if (isset($rec['image'])): ?>
+              <a href="/<?php print $rec['pretty_path']; ?>"><img src="<?php print $rec['image']; ?>"/></a>
+            <?php endif; ?>
+
+            <?php if (isset($rec['title'])): ?>
+              <h3 class="title"><?php print l($rec['title'], $rec['path']); ?></h3>
+            <?php endif; ?>
+
+            <?php if (isset($rec['call_to_action'])): ?>
+              <div class="gallery-description"><?php print $rec['call_to_action']; ?></div>
+            <?php endif; ?>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($more_campaigns_link)): ?>
+    <div class="cta">
+      <div class="wrapper">
+        <?php if (isset($call_to_action)): ?>
+        <h2 class="__message"><?php print $call_to_action; ?></h2>
+        <?php endif; ?>
+
+        <?php //@TODO: Address styles for this slightly different CTA content. ?>
+        <div class="cta-more-campaigns"><?php print $more_campaigns_link; ?></div>
+        <div class="cta-back-to-campaign"><?php if (isset($back_to_campaign_link)): ?><?php print $back_to_campaign_link; ?><?php endif; ?></div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($redeem_form): ?>
+    <a href="#" data-modal-href="#modal-redeem-form"><?php print $redeem_form['link']; ?></a>
+    <div data-modal id="modal-redeem-form" role="dialog">
+      <h2 class="banner"><?php print ($redeem_form['header']); ?></h2>
+      <?php print $redeem_form['copy']; ?>
+      <?php print render($redeem_form['form']); ?>
+    </div>
+    <?php if ($redeem_form['delete_form']): ?>
+      <?php print render($redeem_form['delete_form']); ?>
+    <?php endif; ?>
+  <?php endif; ?>
+
+</section>
