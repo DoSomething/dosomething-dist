a23e2795122f3b72dc9f2a27aaff21d6e736a5ac
Jenkins <admin@dosomething.org>
2014-08-19 09:42:00 -0400

    Release v0.2.210

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/system/partials/footer.tpl.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Generates site-wide global page footer.
+ **/
+
+  // Checks if current site is an international affiliate
+  $is_affiliate = dosomething_settings_is_affiliate();
+?>
+
+<footer class="chrome--footer<?php if ($is_affiliate): ?> -affiliate <?php endif; ?>">
+
+  <?php if (isset($affiliate_logo)): ?>
+  <div class="col branding">
+    <h4><?php print $affiliate_logo['text'] ?></h4>
+    <?php 
+      print theme_image(array(
+        'path'  => $affiliate_logo['file'],
+        'title' => $affiliate_logo['text'],
+        'alt'   => $affiliate_logo['text'],
+      )); 
+    ?>
+  </div>
+  <?php endif; ?>
+
+  <?php print theme('social-networks', array(
+    'social' => $social,
+    'container_classes' => 'tablet',
+  )); ?>
+
+  <?php foreach($links as $column): ?>
+  <div class="col <?php print $column['class']; ?> js-footer-col">
+    <h4><?php print $column['heading']; ?></h4>
+    <?php if (!empty($column['links'])): ?>
+    <ul>
+      <?php foreach($column['links'] as $link): ?>
+        <li><?php print $link; ?></li>
+      <?php endforeach; ?>
+    </ul>
+    <?php endif; ?>
+  </div>
+  <?php endforeach; ?>
+
+  <?php print theme('social-networks', array(
+    'social' => $social,
+    'container_classes' => 'mobile',
+  )); ?>
+
+  <div class="subfooter">
+    <ul class="utility">
+      <li><?php print l("Terms of Service", "node/1049") ?></li>
+      <li><?php print l("Privacy Policy", "node/1050") ?></li>
+    </ul>
+  </div>
+</footer>
