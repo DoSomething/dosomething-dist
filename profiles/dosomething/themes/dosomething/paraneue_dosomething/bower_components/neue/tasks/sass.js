3af307f5d448a7d98c85e3ed26dbe8a0bcf50083
Jenkins <admin@dosomething.org>
2014-09-24 16:48:53 -0400

    Release v0.3.20

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/bower_components/neue/tasks/sass.js
@@ -0,0 +1,11 @@
+module.exports = {
+  compile: {
+    files: {
+      "dist/neue.css": "scss/neue-build.scss",
+    },
+    options: {
+      outputStyle: "compressed",
+      sourceComments: "map"
+    }
+  }
+}
