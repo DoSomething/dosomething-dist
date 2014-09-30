a4586e5ab3aa99a73c926aaa68ab2371272e0d3b
Jenkins <admin@dosomething.org>
2014-09-30 15:08:13 -0400

    Release v0.3.32

---
+++ profiles/dosomething/modules/dosomething/dosomething_campaign/theme/campaign-block.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Returns the HTML for a single Campaign Block.
+ *
+ * Available Variables
+ * - $nid: Node ID for campaign (integer).
+ * - $title: Title of campaign (string).
+ * - $call_to_action: Call to action text for campaign. (string).
+ * - $image: URL path for campaign image (string).
+ * - $path: URL path for campaign node (string).
+ * - $pretty_path: Pretty URL path for campaign node (string).
+ * - $staff_pick: Indicate if this campaign a staff pick (boolean).
+ * - $source: The signup source to pass as a query string param to the $path.
+ */
+?>
