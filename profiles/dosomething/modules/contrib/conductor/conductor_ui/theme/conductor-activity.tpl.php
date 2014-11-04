dfd08b849893f72bf49ebb21ce917dd7440a2157
Jenkins <admin@dosomething.org>
2014-11-04 16:07:21 -0500

    Release v0.3.84

---
+++ profiles/dosomething/modules/contrib/conductor/conductor_ui/theme/conductor-activity.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ */
+?>
+<div class="conductor-ui-activity <?php print check_plain($class); ?>" id="<?php print check_plain($class); ?>">
+  <span><?php print check_plain($title); ?></span>
+  <div class="conductor-drop-link"><?php print $drop_links; ?></div>
+</div>
