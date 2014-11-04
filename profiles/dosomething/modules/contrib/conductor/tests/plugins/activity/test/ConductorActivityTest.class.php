023cb385a7dea9ac5baecc6648ab0e094dc14328
Jenkins <admin@dosomething.org>
2014-11-04 10:32:53 -0500

    Release v0.3.82

---
+++ profiles/dosomething/modules/contrib/conductor/tests/plugins/activity/test/ConductorActivityTest.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This is a test activity to perform different actions depending on the context data.
+ */
+class ConductorActivityTest extends ConductorActivity {
+
+  /**
+   * Implements ConductorActivity::run().
+   */
+  public function run() {
+    if ($this->getState()->getContext($this->name . ':suspend') == TRUE && $this->getState()->getContext($this->name . ':resume') == FALSE) {
+      $this->activityState->markSuspended();
+    }
+    else {
+      $this->activityState->markCompleted();
+    }
+  }
+
+  /**
+   * Implements ConductorActivity::getSuspendPointers().
+   */
+  public function getSuspendPointers() {
+    return array(
+      $this->name . ':suspended',
+    );
+  }
+
+}
