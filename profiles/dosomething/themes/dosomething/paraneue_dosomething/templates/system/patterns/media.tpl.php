29ee267fbfb203d80823d902ce8267d468299417
Jenkins <admin@dosomething.org>
2014-11-25 10:29:13 -0500

    Release v0.3.130

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/system/patterns/media.tpl.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ * Generates the HTML for the Neue Media pattern.
+ *
+ * Available variables:
+ * - $content: An associative array that can contain:
+ *    - $title: Title of the item.
+ *    - $description: Description or subtitle.
+ *    - $impact: Impact number.
+ *    - $image: Thumbnail image.
+ *    - $link: Link to the item.
+ *
+ * @see paraneue_dosomething_get_gallery_item()
+ */
+?>
+<article class="media">
+  <div class="wrapper">
+    <?php if (!empty($content['image'])): ?>
+      <?php print $content['image']; ?>
+    <?php endif; ?>
+  </div>
+
+  <div class="__body">
+    <?php if (!empty($content['title'])): ?>
+      <h3 class="__title">
+        <?php if (!empty($content['link'])): ?>
+          <a href="<?php print $content['link']; ?>">
+            <?php print $content['title']; ?>
+          </a>
+        <?php endif; ?>
+      </h3>
+    <?php endif; ?>
+    <div class="__description">
+      <?php if (!empty($content['impact'])): ?>
+        <p><?php print t('My impact:'); ?>
+        <p><?php print $content['impact']; ?></p>
+      <?php endif; ?>
+      <?php if (!empty($content['description'])): ?>
+        <?php print $content['description']; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+</article>
