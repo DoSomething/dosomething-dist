15a8417f5961f0391bdfb18de7ca4878137a7227
Jenkins <admin@dosomething.org>
2014-12-01 15:58:42 -0500

    Release v0.3.135

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/js/app.js
@@ -0,0 +1,71 @@
+/**
+ * This is where we load and initialize components of our app.
+ */
+define("app", function(require) {
+  "use strict";
+
+  var $ = require("jquery");
+
+  // let's get going
+  var Finder   = require("finder/Finder");
+  var Donate   = require("donate/Donate");
+  var Revealer = require("revealer/Revealer");
+
+  // Initialize modules on load
+  require("neue/main");
+  require("campaign/SchoolFinder");
+  require("campaign/sources");
+  require("campaign/tips");
+  require("campaign/tabs");
+  require("campaign/ImageUploader");
+  require("campaign/ImageCrop");
+  require("validation/auth");
+  require("validation/reportback");
+  require("validation/address");
+  require("validation/donate");
+  require("Analytics");
+  require("tiles");
+
+
+  $(document).ready(function() {
+    var $body = $("body");
+
+    var $campaignFinderForm = $(".js-finder-form");
+    if( $campaignFinderForm.length ) {
+      var $results = $(".js-campaign-results");
+      var $blankSlate = $(".js-campaign-blankslate");
+      Finder.init($campaignFinderForm, $results, $blankSlate);
+    }
+
+
+    var $donateForm = $("#modal--donate-form");
+    if( $donateForm.length ) {
+      Donate.init();
+    }
+
+
+    /**
+     * Show hide large galleries.
+     * Only applies to taxonomy pages for the time being.
+     */
+    var $galleries = $body.find(".gallery.-mosaic");
+
+    if ($body.hasClass("page-taxonomy") && $galleries.length > 0) {
+      var galleryList = [];
+
+      $galleries.each(function (index, gallery) {
+        var $gallery = $(gallery);
+        var $tiles = $gallery.find("li");
+
+        if ($tiles.length > 5) {
+          galleryList[index] = new Revealer($gallery, $tiles, "gallery");
+        }
+      });
+    }
+
+    
+    $("html").addClass("js-ready");
+    
+  });
+
+});
