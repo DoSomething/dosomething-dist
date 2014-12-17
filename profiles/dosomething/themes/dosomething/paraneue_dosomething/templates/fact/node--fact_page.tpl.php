0ab6f4283e646f9d6ad09e6205a788f6147b0a94
Jenkins <admin@dosomething.org>
2014-12-17 15:56:57 -0500

    Release v0.3.150

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/fact/node--fact_page.tpl.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Returns the HTML for the Fact page.
+ *
+ * Available Variables
+ * - $nid: Node ID for fact page (integer).
+ * - $classes: Additional classes passed for output (string).
+ * - $title: Title of fact page (string).
+ * - $subtitle: Subtitle of fact page (string).
+ * - $intro_title: Introductory title of fact page (string).
+ * - $facts: Array of facts for the fact page (array).
+ * - $sources: List of sources for the fact page (string).
+ * - $cta_link: Call To Action link of fact page (string).
+ */
+?>
+
+<article id="node-<?php print $node->nid; ?>" class="fact fact-page <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <header role="banner" class="-basic">
+    <div class="wrapper">
+      <h1 class="__title"><?php print $title; ?></h1>
+      <?php if (isset($subtitle)): ?>
+        <h2 class="__subtitle"><?php print $subtitle; ?></h2>
+      <?php endif; ?>
+    </div>
+  </header>
+
+  <?php if (isset($intro)): ?>
+    <section class="container intro">
+      <div class="wrapper">
+        <?php print $intro; ?>
+      </div>
+    </section>
+  <?php endif; ?>
+
+  <?php if (isset($facts_chunked)): ?>
+    <?php foreach ($facts_chunked as $key_chunk => $fact_chunk): ?>
+      <?php 
+        $start_value = (($key_chunk * 5) + 1); // Start value for list of facts
+        $is_first = ($start_value === 1); // First set of facts
+        $is_last = ($key_chunk === (sizeof($facts_chunked) - 1)); // Last set of facts
+      ?>
+
+      <section class="container container--fact">
+
+        <div class="wrapper">
+
+          <div class="container__body">
+
+            <?php if (isset($intro_image) && $is_first): ?>
+              <aside class="media -inline">
+                 <?php print $intro_image; ?>
+              </aside>
+            <?php endif; ?>
+            
+            <ol start="<?php print $start_value; ?>">
+              <?php foreach ($fact_chunk as $key => $fact): ?>
+                <li><?php print ($start_value + $key) . '. ' . $fact['fact']; ?></li>
+              <?php endforeach; ?>
+            </ol>
+
+          </div>
+
+        </div>
+
+        <?php if (isset($call_to_action) && $is_first): ?>
+          <div class="cta">
+            <div class="wrapper">
+              <h2 class="__message"><?php print $call_to_action; ?></h2>
+              <?php print $cta_link; ?>
+            </div>
+          </div>
+        <?php endif; ?>
+
+        <?php if (isset($sources) && $is_last): ?>
+          <div class="wrapper">
+          <section class="sources">
+            <h3 class="__title js-toggle-sources"><?php print t('Sources'); ?></h3>
+            <ul class="__body legal">
+              <?php foreach ($sources as $key => $source): ?>
+                <li><sup><?php print ($key + 1); ?></sup> <?php print $source; ?></li>
+              <?php endforeach; ?>
+            </ul>
+          </section>
+        </div>
+        <?php endif; ?>
+
+      </section>
+    <?php endforeach; ?>
+  <?php endif; ?>
+
+  <?php if (isset($call_to_action)): ?>
+    <div class="cta">
+      <div class="wrapper">
+        <h2 class="__message"><?php print $call_to_action; ?></h2>
+        <?php print $cta_link; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+  
+</article>
