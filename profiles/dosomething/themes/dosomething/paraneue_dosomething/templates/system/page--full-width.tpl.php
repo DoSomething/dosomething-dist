a746248e1cb9ee14a0f540cc8152abee76487b92
Jenkins <admin@dosomething.org>
2015-01-14 10:14:43 -0500

    Release v0.3.175

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/system/page--full-width.tpl.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Generates minimal site-wide page chrome.
+ * @see https://drupal.org/node/1728148
+ **/
+
+?>
+
+<?php print $variables['navigation']; ?>
+<?php print $variables['header']; ?>
+
+<main role="main">
+  <?php print render($page['content']); ?>
+</main>
+
+<?php print $variables['footer']; ?>
+
