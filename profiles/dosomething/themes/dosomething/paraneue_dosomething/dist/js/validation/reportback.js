009cb9b0c9f1a5f0ef059b8844ecf4e3e09afcd4
Jenkins <admin@dosomething.org>
2015-02-06 15:10:24 -0500

    Release v0.3.213

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/dist/js/validation/reportback.js
@@ -0,0 +1,65 @@
+define(function(require) {
+  "use strict";
+
+  var Validation = require("neue/validation");
+  var $          = require("jquery");
+
+  // validate number of items
+  Validation.registerValidationFunction("positiveInteger", function(string, done) {
+    var trimmedString = string.replace(" ", "");
+    if( trimmedString !== "" && /^[1-9]\d*$/.test(trimmedString) ) {
+      return done({
+        success: true,
+        message: Drupal.t("That's great!")
+      });
+    } else {
+      return done({
+        success: false,
+        message: Drupal.t("Enter a valid number!")
+      });
+    }
+  });
+
+  // validate that string isn't blank
+  Validation.registerValidationFunction("reportbackReason", function(string, done) {
+    if( string !== "" ) {
+      return done({
+        success: true,
+        message: Drupal.t("Thanks for caring!")
+      });
+    } else {
+      return done({
+        success: false,
+        message: Drupal.t("Tell us why you cared!")
+      });
+    }
+  });
+
+  // validate that string isn't blank and less than or equal to 60 characters
+  Validation.registerValidationFunction("caption", function(string, done) {
+    // @TODO: temporary solution until Neue validation script is 
+    // updated to handle client side only forms.
+    var $form = $("#dosomething-reportback-image-form");
+
+    if( string !== "" && string.length <= 60 ) {
+      if ($form.length) {
+        $form.data("validation-passed", true);
+      }
+
+      return done({
+        success: true,
+        message: Drupal.t("Sounds great!")
+      });
+    } else {
+      if ($form.length) {
+        $form.data("validation-passed", false);
+      }
+
+      return done({
+        success: false,
+        message: Drupal.t("Tell us about your photo!")
+      });
+    }
+  });
+
+});
