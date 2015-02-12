ba6494feacf98d90f82a4fb0fde49d08cfb8b142
Jenkins <admin@dosomething.org>
2015-02-12 09:54:07 -0500

    Release v0.3.228

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/system/partials/info-bar.tpl.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Generates info bar footer.
+ **/
+?>
+
+<?php if (isset($zendesk_form) || isset($formatted_partners)): ?>
+<footer class="info-bar">
+  <div class="wrapper">
+
+    <?php if (isset($zendesk_form)): ?>
+    <div class="help">
+      <?php print t('Questions?'); ?> <a href="#" data-modal-href="#modal-contact-form"><?php print t('Contact Us'); ?></a>
+      <div data-modal id="modal-contact-form" class="modal--contact" role="dialog">
+        <h2 class="heading -banner"><?php print t('Contact Us'); ?></h2>
+        <div class="modal__block">
+          <p><?php print $zendesk_form_header; ?></p>
+        </div>
+        <div class="modal__block">
+          <?php print render($zendesk_form); ?>
+        </div>
+      </div>
+    </div>
+    <?php endif; ?>
+
+    <?php if (isset($formatted_partners)): ?>
+      <div class="sponsor">
+      <?php print t("In partnership with"); ?> <?php print $formatted_partners; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</footer>
+<?php endif; ?>
+
