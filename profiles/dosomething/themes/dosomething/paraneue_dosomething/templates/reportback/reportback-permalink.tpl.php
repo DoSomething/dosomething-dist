39e3bdbd03b6a832bf9e7aa3642965496c9489d0
Jenkins <admin@dosomething.org>
2015-03-03 10:54:22 -0500

    Release v0.4.19

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-permalink.tpl.php
@@ -0,0 +1,34 @@
+
+<h2> <?php echo $node->title ?> </h2>
+
+<?php if (!$is_current): ?>
+  <?php echo $node->call_to_action ?>
+
+  <h3> <?php t('The Problem') ?> </h3>
+  <?php echo $node->fact_problem['fact'] ?>
+
+  <h3> <?php t('The Solution') ?> </h3>
+  <?php echo $node->fact_solution['fact'] ?>
+
+<?php endif ?>
+
+<?php echo $rb_image ?>
+
+<?php // Used to determine if the curernt logged in user is the user who submitted the rb. ?>
+<?php if ($is_current): ?>
+  <?php echo $copy_vars['owners_rb_subtitle'] ?>
+  <?php echo $copy_vars['owners_rb_scholarship'] ?>
+  <?php echo check_plain($reportback->caption) ?>
+  <?php echo check_plain($user->first_name) ?>
+
+  <h3>
+  <?php echo $copy_vars['owners_rb_important'] ?>
+ </h3>
+  <?php echo check_plain($reportback->why_participated) ?>
+
+  <?php echo check_plain($reportback->quantity) ?>&nbsp;
+  <?php echo $reportback->quantity_label ?>
+
+
+
+<?php endif ?>
