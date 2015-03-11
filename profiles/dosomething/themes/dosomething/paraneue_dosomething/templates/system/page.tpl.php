243ff61b34c80641a7f9e11f31b147c3e02b79e2
Jenkins <admin@dosomething.org>
2015-03-11 15:54:27 -0400

    Release v0.4.33

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/system/page.tpl.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Generates site-wide page chrome.
+ * @see https://drupal.org/node/1728148
+ **/
+
+?>
+
+<!--[if lt IE 8 ]><div class="messages error">You're using an unsupported browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to make sure everything works nicely!</div><![endif]-->
+
+<?php if (!empty($tabs['#primary'])): ?><nav class="admin-tabs"><?php print render($tabs); ?></nav><?php endif; ?>
+<?php print $messages; ?>
+
+<div class="wrapper">
+  <?php print $variables['navigation']; ?>
+  <?php print $variables['header']; ?>
+
+  <main role="main" class="container -padded">
+    <div class="wrapper">
+      <div class="container__block">
+        <?php print render($page['content']); ?>
+      </div>
+    </div>
+  </main>
+
+  <?php print $variables['footer']; ?>
+</div>
