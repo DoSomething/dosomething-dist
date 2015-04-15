b5d7cb1bc6c4b8e3eadcc855ed0b3774cf9f7ba7
Jenkins <admin@dosomething.org>
2015-04-15 15:47:21 -0400

    Release v0.4.87

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Returns the HTML for the Campaign Pitch page.
+ *
+ * Available Variables
+ * - $campaign: A campaign object. @see dosomething_campaign_load()
+ * - $classes: Additional classes passed for output (string).
+ * - $campaign_scholarship: Scholarship amount (string).
+ */
+?>
+
+<section class="campaign campaign--pitch pitch">
+  <header role="banner" class="header -hero <?php print $classes; ?>">
+    <div class="wrapper">
+      <?php print $campaign_headings; ?>
+
+      <?php if (isset($signup_button_primary)): ?>
+        <div class="header__signup">
+          <?php print render($signup_button_primary); ?>
+          <?php print $campaign_scholarship; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php print $promotions; ?>
+    </div>
+  </header>
+
+  <div class="cta -persistent optimizely-hide-count js-fixedsticky">
+    <div class="wrapper">
+      <?php if (isset($signup_button_secondary)): ?>
+        <div class="cta__button">
+          <?php print $signup_button_secondary; ?>
+        </div>
+      <?php endif; ?>
+      <?php if (isset($campaign->secondary_call_to_action)): ?>
+        <h4 class="cta__message with-count"><?php print $signup_cta; ?></h4>
+        <h4 class="cta__message without-count"><?php print $campaign->secondary_call_to_action; ?></h4>
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <?php if (isset($reportbacks_showcase)): ?>
+    <div class="container -showcase optimizely-hidden">
+      <div class="wrapper">
+        <ul class="gallery -quartet -shuffle">
+          <?php foreach ($reportbacks_showcase['items'] as $index => $reportback): ?>
+            <li><?php print $reportback; ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($campaign->value_proposition)): ?>
+    <div class="container">
+      <div class="wrapper">
+        <div class="container__block -half">
+          <h3><?php print t('The Problem'); ?></h3>
+          <p><?php print $campaign->fact_problem['fact']; ?></p>
+
+          <h3><?php print t('The Solution'); ?></h3>
+          <?php // @TODO: DRY this logic with action page via $campaign ?>
+          <?php if (isset($campaign->fact_solution)): ?>
+            <p><?php print $campaign->fact_solution['fact']; ?></p>
+          <?php elseif (isset($campaign->solution_copy)): ?>
+            <?php print $campaign->solution_copy; ?>
+          <?php endif; ?>
+
+          <?php if (isset($campaign->solution_support)): ?>
+            <?php print $campaign->solution_support; ?>
+          <?php endif; ?>
+        </div>
+
+        <div class="container__block -half">
+          <h3><?php print t('What You Get'); ?></h3>
+          <p><?php print $campaign->value_proposition; ?></p>
+        </div>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($campaign->secondary_call_to_action)): ?>
+    <div class="cta optimizely-hide-count">
+      <div class="wrapper">
+        <h2 class="cta__message with-count"><?php print $signup_cta; ?></h2>
+        <h2 class="cta__message without-count"><?php print $campaign->secondary_call_to_action; ?></h2>
+        <?php if (isset($signup_button_secondary)): ?>
+          <?php print $signup_button_secondary; ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <div class="info-bar -dark">
+    <div class="wrapper">
+      <?php print $tagline; ?>
+      <em><?php print t('*mic drop'); ?></em>
+    </div>
+  </div>
+
+</section>
