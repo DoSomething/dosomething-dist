1bccbe26b82a8a2bc6ec9f074edb64cc746b7466
Jenkins <admin@dosomething.org>
2015-04-15 13:44:27 -0400

    Release v0.4.86

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/js/lib.js
@@ -0,0 +1,22 @@
+// Attach jQuery to the window for backwards compatibility
+import jQuery from 'jquery';
+window.jQuery = jQuery;
+window.$ = jQuery;
+
+// Mailcheck, for email validation & suggestions
+import 'mailcheck';
+
+// jQuery Once plugin
+import 'jquery-once';
+
+// $.unveil plugin for lazy-loading images
+import 'unveil';
+
+// IE8 polyfill for $.ajax CORS support
+import 'jquery.iecors';
+
+// RequestAnimationFrame polyfill
+import 'raf.js';
+
+// Filament Group's fixed-sticky polyfill for `position: sticky`
+import 'fixed-sticky';
