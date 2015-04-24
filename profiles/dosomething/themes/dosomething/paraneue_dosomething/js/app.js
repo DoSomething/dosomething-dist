d131dfee7aad90595969cb8581658c644847d9a7
Jenkins <admin@dosomething.org>
2015-04-24 17:07:00 -0400

    Release v0.4.95

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/js/app.js
@@ -0,0 +1,125 @@
+/**
+ * This is where we load and initialize components of our app.
+ */
+
+import $ from 'jquery';
+import debounce from 'lodash/function/debounce';
+
+import Finder from 'finder/Finder';
+import Donate from 'donate/Donate';
+import Revealer from 'revealer/Revealer';
+import Swapper from 'swapper/Swapper';
+import Reportback from 'reportback/Reportback';
+import Share from 'utils/Share';
+
+import 'dosomething-neue';
+import 'dosomething-validation';
+import Modal from 'dosomething-modal';
+
+// Attach Modal to the window so `<script>` tags can use it.
+window.DSModal = Modal;
+
+import 'campaign/SchoolFinder';
+import 'validators/auth';
+import 'validators/reportback';
+import 'validators/address';
+import 'validators/donate';
+import 'utils/Analytics';
+import 'patterns/tiles';
+
+/**
+ * Initialize modules on load.
+ */
+$(document).ready(function() {
+  // If a fixed-sticky element is on the page, hook it up.
+  $(".js-fixedsticky").fixedsticky();
+
+  var $body = $("body");
+
+  var $campaignFinderForm = $(".js-finder-form");
+  if( $campaignFinderForm.length ) {
+    var $results = $(".js-campaign-results");
+    var $blankSlate = $(".js-campaign-blankslate");
+    Finder.init($campaignFinderForm, $results, $blankSlate);
+  }
+
+
+  var $donateForm = $("#modal--donate-form");
+  if( $donateForm.length ) {
+    Donate.init();
+  }
+
+  var $revealGalleries = $body.find('[data-show-more="true"]');
+  /**
+   * Show hide large galleries.
+   */
+  if ($revealGalleries.length > 0) {
+    var galleryList = [];
+
+    $revealGalleries.each(function (index, gallery) {
+      var $gallery = $(gallery);
+      var $tiles = $gallery.find("li");
+      var isMosaicGallery = $gallery.hasClass('-mosaic');
+      var initialItems = isMosaicGallery ? 5 : 6;
+      var stepItems = isMosaicGallery ? 8 : 6;
+
+      if ($tiles.length > initialItems) {
+        galleryList[index] = new Revealer($gallery, $tiles, "gallery", initialItems, stepItems);
+      }
+    });
+  }
+
+  var $mosaicGalleries = $body.find(".gallery.-mosaic");
+
+  /**
+   * Swap low resolution images for feature tiles in mosaic gallery
+   * with higher resolution version from tablet size up.
+   */
+  if ($mosaicGalleries.length && $mosaicGalleries.hasClass("-featured")) {
+    var swappedImagesList = [];
+
+    $(window).on("resize", debounce(function () {
+
+      if (window.matchMedia("(min-width: 768px)").matches && !swappedImagesList.length) {
+        $mosaicGalleries.each(function (index) {
+          var $featureTile = $(this).find("li").first();
+          swappedImagesList[index] = new Swapper($featureTile);
+        });
+      }
+    }, 500));
+
+  }
+
+  /**
+   * Initialize Reportback js.
+   */
+  var $reportback = $body.find("#reportback");
+
+  if ($reportback.length) {
+    Reportback.init($reportback);
+  }
+
+
+  /*
+   * Show share links in a popup
+   */
+  if ($body.find("a.js-share-link").length) {
+    Share.popup();
+  }
+
+  /*
+   * Toggle truncated text and long text on permalink pages.
+   */
+  var $showMoreText = $body.find(".js-permalink-show-more");
+
+  if ($showMoreText.length) {
+    $showMoreText.click(function(event) {
+      event.preventDefault();
+      $(".participate").toggleClass("is-shown");
+      ($showMoreText.html() === "Show More") ? $showMoreText.html("Show Less") : $showMoreText.html("Show More");
+    });
+  }
+
+  $("html").addClass("js-ready");
+
+});
---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/template.php
@@ -0,0 +1,154 @@
+<?php
+
+// Define theme directory path
+define('PARANEUE_DS_PATH', drupal_get_path('theme', 'paraneue_dosomething'));
+define('LOCAL_ASSET_PATH', '/' . PARANEUE_DS_PATH);
+
+// If ds_version is set, use CDN assets
+if(theme_get_setting('asset_path')) {
+  $cdn_path = theme_get_setting('asset_path');
+  $cdn_path = str_replace('{ds_version}', variable_get('ds_version', 'latest'), $cdn_path);
+  define('DS_ASSET_PATH', $cdn_path);
+} else {
+  define('DS_ASSET_PATH', LOCAL_ASSET_PATH);
+}
+
+// Determine whether to use minified stylesheets or not.
+if(theme_get_setting('use_minified_assets')) {
+  define('DS_STYLE_PATH', DS_ASSET_PATH . '/dist/app.min.css');
+} else {
+  define('DS_STYLE_PATH', DS_ASSET_PATH . '/dist/app.css');
+}
+
+// Define asset directory paths
+define('VENDOR_ASSET_PATH', DS_ASSET_PATH . '/node_modules');
+define('NEUE_ASSET_PATH', VENDOR_ASSET_PATH . '/dosomething-neue');
+
+// Theme includes
+require_once PARANEUE_DS_PATH . '/includes/bootstrap.inc';
+require_once PARANEUE_DS_PATH . '/includes/theme.inc';
+require_once PARANEUE_DS_PATH . '/includes/preprocess.inc';
+require_once PARANEUE_DS_PATH . '/includes/helpers.inc';
+require_once PARANEUE_DS_PATH . '/includes/patterns.inc';
+require_once PARANEUE_DS_PATH . '/includes/form.inc';
+require_once PARANEUE_DS_PATH . '/includes/auth/login.inc';
+require_once PARANEUE_DS_PATH . '/includes/auth/register.inc';
+
+
+/**
+ * We use our own asset pipeline (through Grunt), so we want to bypass most
+ * of Drupal's included assets.
+ *
+ * Implements hook_css_alter().
+ */
+function paraneue_dosomething_css_alter(&$css) {
+  // Load excluded CSS files from theme.
+  $excludes = _paraneue_dosomething_alter(paraneue_dosomething_theme_get_info('exclude'), 'css');
+  $css = array_diff_key($css, $excludes);
+
+  // Force removal of files we can't seem to kill with exclude[css] array
+  // @TODO: Figure out why this is necessary.
+  unset($css[drupal_get_path('module', 'date') . '/date_api/date.css']);
+  unset($css[drupal_get_path('module', 'ctools') . '/css/ctools.css']);
+  unset($css[drupal_get_path('module', 'views') . '/css/views.css']);
+  unset($css[drupal_get_path('module', 'field_group') . '/field_group.field_ui.css']);
+  unset($css[drupal_get_path('module', 'addressfield') . '/addressfield.css']);
+}
+
+/**
+ * We use our own asset pipeline (through Grunt), so we want to bypass most
+ * of Drupal's included assets.
+ *
+ * Implements hook_js_alter().
+ */
+function paraneue_dosomething_js_alter(&$js) {
+  // Get $js_dir path:
+  //    a) DS_ASSET_PATH is local. Path must be built with drupal_get_path()
+  //       function (already assigned to PARANEUE_DS_PATH).
+  //       Using DS_ASSET_PATH in this case is wrong: it starts
+  //       with '/' so Drupal can't open js file for parsing.
+  //       @see drupal_add_js().
+  //    b) DS_ASSET_PATH is not local. It is already a valid URL to CDN.
+  $is_local_asset = DS_ASSET_PATH == LOCAL_ASSET_PATH;
+  $js_dir = $is_local_asset ? PARANEUE_DS_PATH : DS_ASSET_PATH;
+
+  // Add lib.js and app.js using Drupal API:
+  drupal_add_js($js_dir . '/dist/lib.js', array(
+    'group'      => JS_LIBRARY,
+    'weight'     => -200,
+    'every_page' => TRUE,
+    'preprocess' => FALSE,
+  ));
+
+  drupal_add_js($js_dir . '/dist/app.js', array(
+    'group'      => JS_THEME,
+    'weight'     => 999,
+    'every_page' => TRUE,
+    'preprocess' => FALSE,
+  ));
+
+  // Force settings to be embedded after lib, before app scripts.
+  if(isset($js['settings'])) {
+    $inline_settings = array(
+        'type' => 'inline',
+        'group' => JS_LIBRARY,
+        'weight' => 999,
+        'data' => 'jQuery.extend(Drupal.settings, ' . drupal_json_encode(drupal_array_merge_deep_array($js['settings']['data'])) . ");",
+        'every_page' => TRUE,
+      ) + drupal_js_defaults();
+
+    // No need for drupal_get_js() to do this again.
+    unset($js['settings']);
+
+    $js['inline_settings'] = $inline_settings;
+  }
+
+  // Load excluded JS files from theme info file.
+  $excludes = _paraneue_dosomething_alter(paraneue_dosomething_theme_get_info('exclude'), 'js');
+  $js = array_diff_key($js, $excludes);
+
+  // Force removal of files that we can't seem to kill with exclude[js] array
+  unset($js['profiles/dosomething/modules/contrib/devel/devel_krumo_path.js']);
+  unset($js['profiles/dosomething/modules/contrib/google_analytics/googleanalytics.js']);
+}
+
+
+/**
+ * Implements hook_page_alter().
+ */
+function paraneue_dosomething_page_alter(&$page) {
+  // Ensure we have a 'page_bottom' region
+  if (!isset($page['page_bottom'])) {
+    $page['page_bottom'] = array();
+  }
+
+  paraneue_dosomething_page_alter_login($page);
+  paraneue_dosomething_page_alter_register($page);
+}
+
+/**
+ * Implements hook_form_alter();
+ */
+function paraneue_dosomething_form_alter(&$form, &$form_state, $form_id) {
+  paraneue_dosomething_form_alter_base($form, $form_state, $form_id);
+  paraneue_dosomething_form_alter_login($form, $form_state, $form_id);
+  paraneue_dosomething_form_alter_register($form, $form_state, $form_id);
+}
+
+
+/**
+ * Implements hook_head_alter().
+ */
+function paraneue_dosomething_html_head_alter(&$head_elements) {
+  foreach($head_elements as $key => $element) {
+    switch($key) {
+      case (preg_match('/^drupal_add_html_head_link*/', $key)) ? TRUE : FALSE:
+        $shortcut_key = $key;
+        break;
+    }
+  }
+
+  if ($shortcut_key) {
+    unset($head_elements[$shortcut_key]);
+  }
+}
