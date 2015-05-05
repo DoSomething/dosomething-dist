98be240fd306e8480961344aeee869cd709c644a
Jenkins <admin@dosomething.org>
2015-05-05 17:03:53 -0400

    Release v0.4.102

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/partials/campaign-creator.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Snippet for displaying a Campaign's creator property, which is an array.
+ *
+ * For available variables:
+ * @see dosomething_campaign_load().
+ */
+?>
+
+<div class="promotion promotion--creator">
+  <a class="wrapper" href="#" data-modal-href="#modal--creator">
+    <p class="__copy"><?php print t('Created by'); ?></p>
+    <div class="__image">
+      <img src="<?php print $picture['src_square']; ?>" />
+    </div>
+  </a>
+
+  <div data-modal id="modal--creator" class="modal--creator" role="dialog">
+    <a href="#" class="js-close-modal modal-close-button">×</a>
+    <h2 class="heading -banner">The Creator</h2>
+    <div class="wrapper">
+      <div class="__member">
+        <img src="<?php print $picture['src_square']; ?>" />
+      </div>
+      <div class="__body">
+        <h4 class="__title heading -delta"><?php print check_plain($first_name); ?> <?php print $last_initial; ?></h4>
+        <p class="__location"><?php print $city; ?>, <?php print $state; ?></p>
+        <div class="copy"><?php print $copy; ?></div>
+        <div class="form-actions">
+          <a href="#" class="js-close-modal">Back to main page</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
