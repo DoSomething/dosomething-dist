6dda152877ed63e81201195f29d7dff0446a57d1
Jenkins <admin@dosomething.org>
2015-05-20 14:28:01 -0400

    Release v0.4.116

---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/js/revealer/Revealer.js
@@ -0,0 +1,100 @@
+define(function(require) {
+  "use strict";
+
+  var $ = require("jquery");
+
+  /**
+   * Allows for specified grouping of elements to be hidden and then revealed
+   * via an interactive "Show More" button.
+   *
+   * @param {jQuery} $container The container element object that encloses the items to hide.
+   * @param {jQuery} $content   A collection of objects to hide.
+   * @param {string} category   Specify a category type for the content to provide more unique behavior.
+   * @param {int} initialItems  Amount of items to initially display.
+   * @param {int} stepItems     Amount of items to step by
+   */
+  var Revealer = function($container, $content, category, initialItems, stepItems) {
+    this.$container = $container;
+    this.$content   = $content;
+    this.$button    = $("<button class=\"button -tertiary\">" + Drupal.t("Show More") + "</button>");
+    this.category   = category;
+    this.initialItems = initialItems;
+    this.stepItems = stepItems;
+    this.steps      = {
+      // Array of step items to include with each button interaction.
+      items: [],
+
+      // Number of steps.
+      count: 0
+    };
+
+    this.init();
+  };
+
+
+  Revealer.prototype.init = function () {
+    if (this.category === "gallery") {
+      this.prepGalleryItems();
+    }
+    else {
+      this.steps.items.push(this.$content);
+    }
+
+    this.steps.count = this.steps.items.length;
+
+    this.detachContent();
+    this.enableButton();
+  };
+
+
+  Revealer.prototype.detachContent = function () {
+    this.$content.detach();
+  };
+
+
+  Revealer.prototype.reattachContent = function (content) {
+    this.$container.append(content);
+  };
+
+
+  Revealer.prototype.enableButton = function () {
+    this.$button.insertAfter(this.$container);
+
+    var _this = this;
+
+    this.$button.on("click", function () {
+
+      if (_this.steps.count > 0) {
+        _this.reattachContent(_this.steps.items.shift());
+        _this.steps.count--;
+      }
+
+      if (_this.steps.count === 0) {
+        _this.$button.remove();
+      }
+
+    });
+  };
+
+
+  Revealer.prototype.prepGalleryItems = function () {
+    this.$content = this.$content.slice(this.initialItems);
+    var collection = $.makeArray(this.$content);
+
+    // Multiple step reveals.
+    if (this.$content.length > this.stepItems) {
+      while (collection.length > 0) {
+        this.steps.items.push(collection.splice(0, this.stepItems));
+      }
+    }
+    else {
+      this.steps.items.push(collection);
+    }
+
+    collection = null;
+  };
+
+
+  return Revealer;
+
+});
---
+++ profiles/dosomething/themes/dosomething/paraneue_dosomething/templates/campaign/reward-redeem-reportback-count.tpl.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Returns the HTML for the Campaign Reportback Confirmation.
+ *
+ * Available Variables
+ * - $page_title: The page title (string).
+ * - $page_subtitle: (string).
+ * - $page_header: (string).
+ * - $page_copy: (string).
+ * - $page_image: Rendered image (string).
+ * - $form_link: (string).
+ * - $form_header: (string).
+ * - $form_copy: (string).
+ * - $form: Array containing Reward Reedem Form.
+ */
+?>
+
+<article class="campaign campaign--action action">
+
+  <header role="banner" class="header">
+    <div class="wrapper">
+      <h1 class="header__title"><?php print $page_title; ?></h1>
+      <?php if (isset($page_subtitle)): ?>
+      <p class="header__subtitle"><?php print $page_subtitle; ?></p>
+      <?php endif; ?>
+    </div>
+  </header>
+
+  <section class="container -padded">
+    <div class="wrapper">
+      <div class="container__block <?php if (isset($page_image)): print '-half'; else: print '-narrow'; endif; ?>">
+        <?php if (isset($page_header) && !empty($page_header)): ?>
+        <h2 class="inline-sponsor-color"><?php print $page_header; ?></h2>
+        <?php endif; ?>
+        <p><?php print $page_copy; ?></p>
+        <ul class="form-actions -inline -padded">
+          <li><a href="#" data-modal-href="#modal-redeem-form" class="button"><?php print $form_link; ?></a></li>
+        </ul>
+      </div>
+
+      <?php if (isset($page_image)): ?>
+        <div class="container__block -half">
+          <aside>
+            <?php print $page_image; ?>
+          </aside>
+        </div>
+      <?php endif; ?>
+
+      <?php // "Free T-shirts" Modals ?>
+      <div data-modal id="modal-redeem-form" role="dialog">
+        <h2 class="heading -banner"><?php print ($form_header); ?></h2>
+        <div class="modal__block">
+          <?php print $form_copy; ?>
+        </div>
+        <div class="modal__block">
+          <?php print render($form); ?>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</article>
